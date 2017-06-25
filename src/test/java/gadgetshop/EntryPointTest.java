cabd8ef0d938e785bc8ec129eb3b01acd4b64907
Lodo4ka <doomymax@gmail.com>
2017-06-25 20:59:23 +0300

    please fix my project

---
+++ src/test/java/gadgetshop/EntryPointTest.java
@@ -0,0 +1,26 @@
+package gadgetshop;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by lodo4ka on 25/06/2017.
+ */
+public class EntryPointTest {
+
+
+
+    @Test
+    public void main() throws Exception {
+
+
+
+
+    }
+
+    @Test
+    public void start() throws Exception {
+    }
+
+}
